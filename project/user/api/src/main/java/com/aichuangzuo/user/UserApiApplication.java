fd8b5cb415106496fd8b3288f665d27b80afeec0
panyong <panyong@secsmart.net>
2026-07-06 16:04:59 +0800

    feat(hotsearch): 添加每日热搜定时抓取任务

--- project/user/api/src/main/java/com/aichuangzuo/user/UserApiApplication.java
+++ project/user/api/src/main/java/com/aichuangzuo/user/UserApiApplication.java
@@ -2,7 +2,9 @@ package com.aichuangzuo.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class UserApiApplication {
     public static void main(String[] args) {
